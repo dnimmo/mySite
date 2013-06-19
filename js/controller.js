bbc75b5dc87d96d3ef1f5e7c0a2cbeaab6a2fe87
David Nimmo <dnimmo@gmail.com>
2013-06-19 22:14:47 +0100

    Added colour setting

--- js/controller.js
+++ js/controller.js
@@ -13,47 +13,18 @@ function WorkCtrl ($scope) {
 }
 
 //Show settings bar on click
-function FooterCtrl ($scope, $rootScope){
-    $scope.settings = false;
-    $scope.showSettings = function(){
-        $scope.settings = true;
+function SiteCtrl ($scope){
+    $scope.settingsPanel = false;
+    $scope.geocities = false;
+    $scope.geoOn = false;
+    $scope.geoOff = true;
+    $scope.showSettings = function(show){
+        $scope.settingsPanel = show;
     }
-    $scope.hideSettings = function(){
-        $scope.settings = false;
-    }
-
     //Geocities Mode
-    $rootScope.geocities = false;
-    $rootScope.geocitiesModeOn = function(){
-        $rootScope.geocities = true;
-    }
-    $rootScope.geocitiesModeOff = function(){
-        $rootScope.geocities = false;
+    $scope.geocitiesMode = function(geoOn, geoOff){
+        $scope.geocities = geoOn;
+        $scope.geoOn = geoOn;
+        $scope.geoOff = geoOff;
     }
 }
-
-
-function List ($scope){
-    $scope.people = [
-        {name: "David", age: 28},
-        {name: "Victoria", age: 24},
-        {name: "Dante", age: 1},
-        {name: "Juri", age: 1},
-        {name: "Aeris", age: 8},
-        {name: "Nadia", age: 9}
-    ];
-
-    $scope.add = function () {
-        $scope.people.push({
-            name: $scope.new_name,
-            age: $scope.new_age
-        });
-        $scope.new_name="";
-        $scope.new_age="";
-    };
-
-    $scope.remove = function(index){
-        $scope.people.splice(index, 1);
-    }
-};
-
